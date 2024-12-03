9393390ac58c067ead344b3ac35615f492dbf867
braydonlu <lu.braydon@gmail.com>
2024-12-02 18:58:43 -0800

    "observation zone trajectory in meep meep testing"

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -8,6 +8,8 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
     static int pos_multiplier = -1;
+    static double botWidthHalf = 7.25;
+    static double botLengthHalf = 7.5;
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
@@ -16,23 +18,24 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(pos_multiplier*(6.085+0), pos_multiplier*(-3.75+72), Math.PI))
-                //Deposit Preload
-                .setTangent(-Math.PI/2)
-                .lineToY(pos_multiplier*(22+3.75))
-                //Get First Sample
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(pos_multiplier*-botLengthHalf, pos_multiplier*62.5, 0))
+                .setTangent(Math.PI/2)
+                .splineToConstantHeading(new Vector2d(pos_multiplier*(2-botLengthHalf),pos_multiplier*(18+botWidthHalf)),Math.PI/2)
                 .lineToY(pos_multiplier*(27+3.75))
-                .splineToLinearHeading(new Pose2d(pos_multiplier*34,pos_multiplier*28.375,Math.PI), Math.PI/2)
-                .setTangent(Math.PI)
-                .lineToX(pos_multiplier*(36))
-                //Go to Basket
-                .splineToLinearHeading(new Pose2d(pos_multiplier*55.0294,pos_multiplier*55.0294,5*Math.PI/4), Math.PI)
-                .splineToLinearHeading(new Pose2d(pos_multiplier*45,pos_multiplier*28.375,Math.PI), Math.PI/2)
-                .splineToLinearHeading(new Pose2d(pos_multiplier*55.0294,pos_multiplier*55.0294,5*Math.PI/4), Math.PI)
-                .splineToLinearHeading(new Pose2d(pos_multiplier*48,pos_multiplier*48,Math.PI), Math.PI)
-                .splineToLinearHeading(new Pose2d(pos_multiplier*53,pos_multiplier*28.375,Math.PI), Math.PI/2)
-                .splineToLinearHeading(new Pose2d(pos_multiplier*55.0294,pos_multiplier*55.0294,5*Math.PI/4), Math.PI)
-                .splineToLinearHeading(new Pose2d(pos_multiplier*19,pos_multiplier*15,0), -Math.PI/2)
+                .splineToLinearHeading(new Pose2d(-32*pos_multiplier, 36*pos_multiplier, 0), Math.PI/2)
+                .splineToSplineHeading(new Pose2d(-48*pos_multiplier,16*pos_multiplier,0), -Math.PI/2)
+                .setTangent(-Math.PI/2)
+                .lineToY(60*pos_multiplier)
+                .setTangent(Math.PI/2)
+                .lineToY(12*pos_multiplier)
+                .splineToLinearHeading(new Pose2d(-58*pos_multiplier, 16*pos_multiplier,0),-Math.PI/2)
+                .setTangent(Math.PI/2)
+                .lineToY(64*pos_multiplier)
+                .setTangent(Math.PI/2)
+                .lineToY(12*pos_multiplier)
+                .splineToLinearHeading(new Pose2d(-63*pos_multiplier, 16*pos_multiplier,0),-Math.PI/2)
+                .setTangent(Math.PI/2)
+                .lineToY(64*pos_multiplier)
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_OFFICIAL)
