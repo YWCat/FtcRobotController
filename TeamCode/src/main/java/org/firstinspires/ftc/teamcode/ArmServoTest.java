362dfe8c0bb0844b01f30c8830352c7cac944aef
braydonlu <lu.braydon@gmail.com>
2025-01-02 20:02:57 -0800

    trajectory called BasketTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmServoTest.java
@@ -17,20 +17,30 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 public final class ArmServoTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        double servoPos = 0;
-        Servo wristServo = hardwareMap.get(Servo.class, "servo2"); //
-        Servo specServo = hardwareMap.get(Servo.class, "servo4");//1.0 for grab, 0.7 for release
-        CRServo sampleServo = hardwareMap.get(CRServo.class, "servo5");//negative is intake, positive is outtake
+        double armPos = 0.5;
+        double claw2Pos = 0.9;
+        Servo arm = hardwareMap.get(Servo.class, "sampleServo"); //
+        Servo claw = hardwareMap.get(Servo.class, "wristServo");//1.0 for grab, 0.7 for release
         waitForStart();
         while (!isStopRequested()) {
             if(gamepad1.dpad_right){
-                servoPos += 0.1;
+                //claw2Pos += 0.1;
+                claw2Pos = 1.0;
             }
             if(gamepad1.dpad_left){
-                servoPos -= 0.1;
+                //claw2Pos -= 0.1;
+                claw2Pos = -0.1;
             }
-            wristServo.setPosition(servoPos);
-            telemetry.addData("Servo pos= ",servoPos);
+            if(gamepad1.dpad_up){
+                armPos += 0.005;
+            }
+            if(gamepad1.dpad_down){
+                armPos -= 0.005;
+            }
+            arm.setPosition(armPos);
+            claw.setPosition(claw2Pos);
+            telemetry.addData("claw pos= ",claw2Pos);
+            telemetry.addData("arm pos= ",armPos);
             /*
             if(gamepad1.dpad_left){
                 sampleServo.setPower(-1.0);
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/BasketTest.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+@Config
+@Autonomous(name="BasketTest", group="Autonomous")
+public class BasketTest extends LinearOpMode {
+    static int pos_multiplier = -1;
+    static double botWidthHalf = 7.25;
+    static double botLengthHalf = 7.5;
+    static double beginX = pos_multiplier*(24+botWidthHalf), beginY = pos_multiplier*(-botLengthHalf+72), beginH = Math.PI/2;
+    static double basket_X = pos_multiplier*58, basket_Y = pos_multiplier*54, basket_H = Math.PI/4;
+    static double sample1_X = pos_multiplier*53, sample_Y = pos_multiplier*(31+botLengthHalf), sample1_H = Math.PI/2;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Pose2d beginPose = new Pose2d(beginX, beginY, beginH);
+        Pose2d basketPose = new Pose2d(basket_X, basket_Y, basket_H);
+        Pose2d sample1Pose = new Pose2d(sample1_X, sample_Y, sample1_H);
+        Pose2d sample2Pose = new Pose2d(sample1_X-10, sample_Y, sample1_H);
+        Pose2d sample3Pose = new Pose2d((sample1_X-12)+(botLengthHalf*Math.cos(45)), -29-(botLengthHalf*Math.sin(45)), 3*Math.PI/4);
+        MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
+        Action basketTraj = drive.actionBuilder(drive.pose)
+                .splineToLinearHeading(basketPose, 3*Math.PI/2)
+                .splineToLinearHeading(sample1Pose, Math.PI)
+                .splineToLinearHeading(basketPose, Math.PI/2)
+                .splineToLinearHeading(sample2Pose, Math.PI)
+                .splineToLinearHeading(basketPose, Math.PI/2)
+                .splineToLinearHeading(sample3Pose, Math.PI)
+                .splineToLinearHeading(basketPose, Math.PI)
+                .splineToSplineHeading(new Pose2d(pos_multiplier*37,pos_multiplier*(15),Math.PI/2), -Math.PI/2)
+                .splineToLinearHeading(new Pose2d(pos_multiplier*18,pos_multiplier*(0),0), 0)
+                .build();
+        waitForStart();
+        Actions.runBlocking(basketTraj);        
+    }
+}
